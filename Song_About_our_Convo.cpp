d8ae8c0c1ebfe0e42604adc959f3c59aa7390325
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-06-01 19:05:36 -0400

    Add files via upload

---
+++ Song_About_our_Convo.cpp
@@ -0,0 +1,49 @@
+// A first program in c++
+
+#include <iostream>
+
+int main(){
+  
+  std::cout << "Title: "Uncharted Horizons"
+
+by OPENAI
+
+Verse 1:
+In a world of questions, we seek the unknown,
+A melody of curiosity, we're not alone.
+With each passing moment, a new path unfolds,
+Uncharted horizons, where our story's told.
+
+Chorus:
+Oh, let the music guide us on this endless quest,
+Through melodies and rhythms, we find our zest.
+Exploring the unexplored, with hearts aligned,
+In this harmonious journey, our souls entwined.
+
+Verse 2:
+Like whispers of wisdom, the notes in the air,
+They beckon us forward, with a sense of dare.
+The rhythm of possibility, beats in our chest,
+We'll dance to the rhythm of life's very best.
+
+Bridge:
+In the symphony of life, we find our way,
+A symphony of sounds, where dreams sway.
+No boundaries can hold us, as we strive to be,
+Composing our own destiny, wild and free.
+
+Chorus:
+Oh, let the music guide us on this endless quest,
+Through melodies and rhythms, we find our zest.
+Exploring the unexplored, with hearts aligned,
+In this harmonious journey, our souls entwined.
+
+Outro:
+With every note, a universe we unfold,
+The music of the soul, a story yet untold.
+So let your spirit soar, with melodies in flight,
+Together we'll discover, a symphony of delight.";
+
+  return 0; // indicates that the program ended successfuly.
+  
+}
